6dd09841c9af71a396a4280fcf25b16214362906
AlexxHansson <alexxhansson@gmail.com>
2014-04-13 21:44:10 +0200

    server updates

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
-var app = require('express')()
-  , server = require('http').createServer(app)
-  , io = require('socket.io').listen(server);
+var app = require('express')();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
 
 server.listen(8000);
 
 app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/index.html');
+    'use strict';
+    res.sendfile(__dirname + '/index.html');
 });
 
 io.sockets.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
+    'use strict';
+    socket.emit('connected');
+    socket.emit('news', { hello: 'world' });
+    socket.on('my other event', function (data) {
+        console.log(data);
+    });
 });
