bcf84c7b2557d5cde4fb8c366ac06f62bb488c1d
AlexxHansson <alexxhansson@gmail.com>
2014-04-13 23:36:47 +0200

    map fixes

--- app/scripts/routers/main.js
+++ app/scripts/routers/main.js
@@ -26,13 +26,16 @@ define([
                 socket.emit('my other event', { my: 'data' });
             });
 
+            console.log('finding location...');
             navigator.geolocation.getCurrentPosition(function(response){
+                console.log('location found.');
                 User.set('coords', response.coords);
+
                 var mapOptions = {
                     center: new google.maps.LatLng(User.get('coords').latitude, User.get('coords').longitude),
                     zoom: 16
                 };
-                var map = new google.maps.Map(document.getElementById("map-canvas"),mapOptions);
+                var map = new google.maps.Map(document.getElementById('map-canvas'),mapOptions);
 
                 google.maps.event.addDomListener(window, 'load', this);
             });
